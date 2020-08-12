e9d261f575a03eb79dc2a32ec1a00f1fb9abde71
prudhvi260 <prudhvinelavelli9@gmail.com>
2020-08-12 12:43:42 +0530

    project is created

---
+++ SBIAmazonProblem/src/SBIAmazonProblem.java
@@ -0,0 +1,17 @@
+
+public class SBIAmazonProblem {
+	
+  public static void main(String []arg)
+  {
+	  String s="";
+	  int ch[]= {3114,3139,3111,3135,3137};
+	  for(int i=0;i<ch.length;i++)
+	  {
+		  char c=(char)ch[i];
+		  s=s+c;
+	  }
+	  System.out.print(s);
+	  
+	  
+  }
+}
